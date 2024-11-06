1e3e51ba94a305b1c0767f3150b03514706ca2ef
Néphélim <nephcohen@gmail.com>
2024-11-06 16:03:12 +0100

    fluzzle 0.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-import { useState } from 'react';
-import {Link} from ReactDOM;
+import { Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
       <h1>Fluzlle</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
+        <button>Popover</button>
         <p>
-          Edit{' '}
-          <Link href="https://fluzzle.youcodeuse.com">
+          <a
+            href="https://fluzzle.youcodeuse.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Fluzzle Documentation
-          </Link>
+          </a>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
-}
-
+};
 export default App;
