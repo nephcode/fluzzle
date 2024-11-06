de41cf6839e923b3ecea8174203e752aa4000def
Néphélim <nephcohen@gmail.com>
2024-11-06 15:52:22 +0100

    dev 0.1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { port: 7026 },
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
